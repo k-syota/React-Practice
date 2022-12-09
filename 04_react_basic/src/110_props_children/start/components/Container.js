8a46aad8309c455840af5dcb6724b60916c0beb3
Syota Nishimura <syota4723@gmail.com>
2022-12-09 09:39:53 +0900

    :sparkles: props.childrenについて

--- 04_react_basic/src/110_props_children/start/components/Container.js
+++ 04_react_basic/src/110_props_children/start/components/Container.js
@@ -1,9 +1,9 @@
 import "./Container.css";
 
-const Container = ({ title }) => {
+const Container = ({ title,children }) => {
   return (
     <div className="container">
-      <h3>{title}</h3>
+      <h3>{children}</h3>
     </div>
   );
 };
