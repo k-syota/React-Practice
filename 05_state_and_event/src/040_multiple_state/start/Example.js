f8f82382bc8c0fcd43fdd09491da03462e36cd24
Syota Nishimura <syota4723@gmail.com>
2022-12-09 17:43:24 +0900

    :sparkles: stateに関して

--- 05_state_and_event/src/040_multiple_state/start/Example.js
+++ 05_state_and_event/src/040_multiple_state/start/Example.js
@@ -1,5 +1,28 @@
+import { useState } from "react";
+
 const Example = () => {
-  
+    const [countA , setCountA] = useState(0);
+    const [countB , setCountB] = useState(10);
+    const [countC , setCountC] = useState(100);
+    return(
+        <>
+        <p>Aボタンを{countA}回押しました</p>
+        <button onClick={()=> {
+            let val = countA + 1;
+            setCountA(val);
+        }}>Aクリック</button>
+        <p>Bボタンを{countB}回押しました</p>
+        <button onClick={()=> {
+            let val = countB + 1;
+            setCountB(val);
+        }}>Bクリック</button>
+        <p>Cボタンを{countC}回押しました</p>
+        <button onClick={()=> {
+            let val = countC + 1;
+            setCountC(val);
+        }}>Cクリック</button>
+        </>
+    );
 };
 
 export default Example;
