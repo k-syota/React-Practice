4b76d27250e72030b8e673c0bce3520978a28ae6
Syota Nishimura <syota4723@gmail.com>
2022-12-09 09:22:04 +0900

    :sparkles: propsの練習問題

--- 04_react_basic/src/100_practice_props/start/components/Profile.js
+++ 04_react_basic/src/100_practice_props/start/components/Profile.js
@@ -1,9 +1,11 @@
 import "./Profile.css";
 
-const Profile = (props) => {
+const Profile = ({name = 'Jone Doe',age = '??',country = 'Japan'}) => {
   return (
     <div className="profile">
-
+      <h3>name : {name}</h3>
+      <h3>age : {age}</h3>
+      <h3>from : {country}</h3>
     </div>
   );
 };
