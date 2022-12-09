012ec09ce89e435676ec58b6e7e0cfa609666504
Syota Nishimura <syota4723@gmail.com>
2022-12-09 11:52:05 +0900

    :sparkles: イベントに合わせた関数の実行

--- 05_state_and_event/src/010_eventlistener/start/Example.js
+++ 05_state_and_event/src/010_eventlistener/start/Example.js
@@ -1,6 +1,12 @@
 const Example = () => {
+  const clickHandler = () => {
+    alert('test');
+  }
   return (
-    <></>
+    <>
+    <button onClick={clickHandler}>click</button>
+    <button>click</button>
+    </>
   );
 };
 
