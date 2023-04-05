00c3d82dbf6de9c239497409f2f13f8737c17e90
Syota Nishimura <syota4723@gmail.com>
2023-04-05 23:05:24 +0900

    :sparkles: 配列のステートを扱う時の注意点

--- 05_state_and_event/src/064_state_array/start/Example.js
+++ 05_state_and_event/src/064_state_array/start/Example.js
@@ -1,9 +1,20 @@
+import {useState} from 'react'
+
 const Example = () => {
   const numArray = [1, 2, 3, 4, 5];
+  const [nums, setNums] = useState(numArray);
+
+  const shuffle = () => {
+    const newNums = [...nums];
+    const val = newNums.pop();
+    newNums.unshift(val);
+    setNums(newNums);
+  }
 
   return (
     <>
-      
+      <h1>{nums}</h1>
+      <button onClick={shuffle}>shuffle</button>
     </>
   );
 };
