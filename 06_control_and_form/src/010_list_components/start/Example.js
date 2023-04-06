1abb358caeb08c4ecaa7447b93313dfdc3d837b6
Syota Nishimura <syota4723@gmail.com>
2023-04-06 16:08:19 +0900

    :sparkles: リストにキーを設定する

--- 06_control_and_form/src/010_list_components/start/Example.js
+++ 06_control_and_form/src/010_list_components/start/Example.js
@@ -2,12 +2,13 @@
 const animals = ["Dog", "Cat", "Rat"];
 
 const Example = () => {
-
+  const animalList = animals.map((animal) => <li key={animal}>{animal}</li>);
   return (
     <>
       <h3>配列の操作</h3>
       <ul>
-        {animals.map((animal) => <li>{ animal}</li>)}
+        {/* {animals.map((animal) => <li key={animal}>{animal}</li>)} */}
+        {animalList}
       </ul>
     </>
   );
