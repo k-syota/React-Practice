f52c9d31e6f500ff2e7c7e34fb1d52d0b6400a81
Syota Nishimura <syota4723@gmail.com>
2023-04-14 10:41:25 +0900

    :sparkles: 純粋関数について知る

--- 10_functional_programming/src/020_pure_function/start/Example.js
+++ 10_functional_programming/src/020_pure_function/start/Example.js
@@ -1,8 +1,17 @@
 const Example = () => {
+
+  const val1 = 1;
+  const val2 = 2;
+
+  const add = (val1, val2) => {
+    return val1 + val2;
+  }
+
   return (
     <>
       <h3>純粋関数</h3>
       <p>fn(決まった引数) には 決まった戻り値 を返す</p>
+      <div>{add(val1, val2)}</div>
     </>
   );
 };
