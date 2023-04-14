7568c156db88b82006cde51754700f449d398083
Syota Nishimura <syota4723@gmail.com>
2023-04-14 11:27:46 +0900

    :sparkles: 不変性について

--- 10_functional_programming/src/030_immutability/start/Example.js
+++ 10_functional_programming/src/030_immutability/start/Example.js
@@ -1,4 +1,17 @@
 const Example = () => {
+
+  const num = { val: 2 };
+  const double = (num) => {
+    const newNum = {};
+    newNum.val = num.val * 2;
+    return newNum;
+    // 不変性が保持されない例
+    // num.val = num.val * 2;
+    // return num;
+  }
+
+  const newNum = double(num);
+  console.log('newNum', newNum, 'num', num);
   return (
     <>
       <h3>不変性（Immutability）</h3>
