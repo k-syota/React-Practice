897d88bf622272aa5edb77741f58a65542b2a768
Syota Nishimura <syota4723@gmail.com>
2023-04-14 12:01:22 +0900

    :sparkles: Reactと純粋関数

--- 10_functional_programming/src/040_react_pure_fn/start/Example.js
+++ 10_functional_programming/src/040_react_pure_fn/start/Example.js
@@ -1,7 +1,17 @@
+let value = 0;
+
+const ChildPure = ({ value }) => {
+  return <div>{ value}</div>
+}
+
 const Example = () => {
 
+  let value = 0;
+
   return (
+
     <>
+      <ChildPure value={++value } />
     </>
   );
 };
