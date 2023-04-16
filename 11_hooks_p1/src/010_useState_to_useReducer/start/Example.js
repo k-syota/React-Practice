6be2d359a69c2e74b7aa4a4f4bc3ee8c18ea4cb5
Syota Nishimura <syota4723@gmail.com>
2023-04-16 16:24:15 +0900

    :sparkles: useReducerを使ってみる

--- 11_hooks_p1/src/010_useState_to_useReducer/start/Example.js
+++ 11_hooks_p1/src/010_useState_to_useReducer/start/Example.js
@@ -1,7 +1,48 @@
+import {useState, useReducer} from 'react'
+
 const Example = () => {
   
+  const [state, setState] = useState(0);
+  const [rstate, dispatch] = useReducer((prev, {type}) => {
+
+    switch (type) {
+      case "+":
+        return ++prev
+      case "-":
+        return --prev
+      default:
+        throw new Error('不明なactionです')
+    }
+
+    // if (action === '+') {
+    //   return ++prev;
+    // } else if(action === '-'){
+    //   return --prev;
+    // }
+  }, 0)
+
+  const add = () => {
+    setState(prev => ++prev)
+  }
+  const addReducer = () => {
+    dispatch({type:'+'})
+  }
+  const downReducer = () => {
+    dispatch({type:'-'})
+  }
   return (
     <>
+      <div>
+        <h3>useState</h3>
+        <span>{state} : </span>
+        <button onClick={add}>+</button>
+      </div>
+      <div>
+        <h3>useReducer</h3>
+        <span>{rstate} : </span>
+        <button onClick={addReducer}>+</button>
+        <button onClick={downReducer}>-</button>
+      </div>
     </>
   );
 };
