870a34810f4b559bbed2dd5912c783e7aec4ffa4
Syota Nishimura <syota4723@gmail.com>
2023-04-19 16:04:21 +0900

    :sparkles: useCOntextをリファクタリングする

--- 11_hooks_p1/src/050_context_file/start/components/Main.js
+++ 11_hooks_p1/src/050_context_file/start/components/Main.js
@@ -1,5 +1,14 @@
+import { useContext } from "react";
+import { ThemeContext } from "../Example";
+import { useTheme } from "../context/ThemeContext";
+
 const Main = () => {
-  
+    const [theme] = useTheme();
+    return (
+        <main className={`content-${theme}`}>
+        <h1>テーマの切り替え</h1>
+      </main>
+  )
 };
 
 export default Main;
